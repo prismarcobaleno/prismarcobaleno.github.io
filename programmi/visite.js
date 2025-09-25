988034a7c479eeb4a4f0d25a2d4f67421ae43a55
quercia <tu@esempio.com>
2025-09-25 20:13:47 +0200

    altrove + visite

--- programmi/visite.js
+++ programmi/visite.js
@@ -1,58 +1,45 @@
-
-	const dx = new Date() // today, now
-	newdate = dx.toISOString().slice(0, 10); // YYYY-MM-DD
-
-	var sesid = Math.random().toString(36).substr(2, 9)+'X'+Math.random().toString(36).substr(2, 9);
-	var sesidDate = "2025-09-25";
-
-	var parts = location.hostname;
-	var sndleveldomain = parts.replace("http://", "");	
-	var sndleveldomain = sndleveldomain.replace("https://", "");	
-	var sndleveldomain = sndleveldomain.replace("www.", "");
-
-
-  function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";domain="+ sndleveldomain +";path=/";
-  }
-
-
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = document.cookie;
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+expiration = new Date;
+expiration.setMonth(expiration.getMonth()+6)
+counter = eval(cookieVal("total_visited"))
+counter++
+document.cookie = "total_visited="+counter+";expires=" + expiration.toGMTString()
+ 
+ 
+    function cookieVal(cookieName) {
+            thisCookie = document.cookie.split("; ")
+            for (i=0; i<thisCookie.length; i++){
+                    if (cookieName == thisCookie[i].split("=")[0]){
+                            return thisCookie[i].split("=")[1]
+                    }
+            }
+            return 0;
     }
-    return "";
-  }
 
-	var accSesId="";
-	var myCookie = getCookie('gnrcounter_2419_2419');
-	var newgnrCounter2 = getCookie('gnrcounter2_2419_2419');	
-
-	if (myCookie == "") {
-		//alert("cookie does not exist");
-		setCookie('gnrcounter_2419_2419', sesid, 7);
-		setCookie('gnrcounter2_2419_2419', sesidDate, 7);
-		
-		accSesId="0*"+sesidDate+"*"+sesid;
-	} else {
-		// alert("cookie exists");
-		accSesId="1*"+newgnrCounter2+"*"+myCookie;	
-		setCookie('gnrcounter2_2419_2419', sesidDate, 7);		
-		
-	}
-	// alert(accSesId);
 	
+
+if(!Histats_variables)
+    {var Histats_variables=[];} 
+Histats_variables.push("tags","a6Z55wyz5wB4CZZc"); 
+Histats_variables.push("a6Z55wyz5wB4CZZc",""); 
+var _Hasync= _Hasync|| [];
+    _Hasync.push(['Histats.start', '1,4874638,4,0,0,0,00010000']);
+    _Hasync.push(['Histats.fasi', '1']);
+    _Hasync.push(['Histats.track_hits', '']); 
+    (function() { var hs = document.createElement('script'); 
+    hs.type = 'text/javascript'; 
+    hs.async = true; hs.src = ('//s10.histats.com/js15_as.js');
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs); })(); 
+    
+if(!Histats_variables){var Histats_variables=[];} 
+    Histats_variables.push("tags","a6Z55wyz5wB4CZZc"); 
+    Histats_variables.push("a6Z55wyz5wB4CZZc",""); 
+    var _Hasync= _Hasync|| []; _Hasync.push(['Histats.start', '1,2998001,4,0,0,0,00010000']); _Hasync.push(['Histats.fasi', '1']); _Hasync.push(['Histats.track_hits', '']); 
+    (function() { var hs = document.createElement('script'); 
+    hs.type = 'text/javascript'; hs.async = true; hs.src = ('//s10.histats.com/js15_as.js'); 
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs); })(); 
+
+
+
 	var userLang = navigator.language || navigator.userLanguage; 
 	var hostname = window.location.hostname;
 	var refurl = document.referrer;	
@@ -61,9 +48,13 @@
 	var url = document.URL;
 	var screen =screen.width + "x" + screen.height ; 
 	
-	document.write('<script src="https://gnrcounter.com/counterClient.php?accDomainId=78f01f874f42d49e6e015d6be58bfa18&accSesId='+accSesId+'&lng='+userLang+'&hostname='+hostname+'&screen='+screen+'&url='+url+'&title='+title+'&refurl='+refurl+'&platform='+platform+'" type="text/javascript"></script>');  
+    let visite = 0;
+    document.addEventListener('DOMContentLoaded', function() { visite++; });
+
+
+	//document.write('<script src="https://gnrcounter.com/counterClient.php?accDomainId=78f01f874f42d49e6e015d6be58bfa18&lng='+userLang+'&hostname='+hostname+'&screen='+screen+'&url='+url+'&title='+title+'&refurl='+refurl+'&platform='+platform+'" type="text/javascript"></script>');  
     
-	document.write('<div style="flex-grow: 2" class="gnrCounterx scatolina"><div>Visite di oggi: <span class="gnrCounter-boldx">3</span> </div><div>Visite totali: <span class="gnrCounter-boldx">1</span></div>');
+	document.write('<div style="flex-grow: 2" class="gnrCounterx scatolina"><div>Visite di oggi: <span class="gnrCounter-boldx">Boh</span> </div><div>Il tuo schermo: <span class="gnrCounter-boldx">'+ screen +'</span></div>');
 	
  	
 
